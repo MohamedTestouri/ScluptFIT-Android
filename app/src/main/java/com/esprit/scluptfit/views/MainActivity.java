7b6208cbda04b1e558a3cd326d14f6979e8a17c9
Mohamed Testoury <mohamed.testouri@esprit.tn>
2020-11-14 21:46:39 +0100

    init

--- app/src/main/java/com/esprit/scluptfit/views/MainActivity.java
+++ app/src/main/java/com/esprit/scluptfit/views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.esprit.scluptfit;
+package com.esprit.scluptfit.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.esprit.scluptfit.R;
 
 public class MainActivity extends AppCompatActivity {
 
