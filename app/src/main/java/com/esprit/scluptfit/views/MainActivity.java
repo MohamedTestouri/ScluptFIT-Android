f07d18200749f5206acb10a2cb405d45ef42cff0
wassimsefi <wassimsefi@gmail.com>
2020-11-16 16:03:41 +0100

    first commpit

--- app/src/main/java/com/esprit/scluptfit/views/MainActivity.java
+++ app/src/main/java/com/esprit/scluptfit/views/MainActivity.java
@@ -2,15 +2,41 @@ package com.esprit.scluptfit.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.esprit.scluptfit.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static  int SPLASH_SCREEN = 2000;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        image =findViewById(R.id.img);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,StartActivity.class);
+                Pair[] pairs=new Pair[1];
+                pairs[0]=new Pair<View,String>(image,"logo_image");
+                ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+            }
+        },SPLASH_SCREEN) ;
+
+
     }
+
+
 }
