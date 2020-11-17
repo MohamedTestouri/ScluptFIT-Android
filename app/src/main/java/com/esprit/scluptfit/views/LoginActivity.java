6d794094a84bea23e69b2f08e465c8d0d6aa42e5
wassimsefi <wassimsefi@gmail.com>
2020-11-17 17:43:54 +0100

    first commpit

--- app/src/main/java/com/esprit/scluptfit/views/LoginActivity.java
+++ app/src/main/java/com/esprit/scluptfit/views/LoginActivity.java
@@ -9,7 +9,10 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.esprit.scluptfit.R;
--- app/src/main/java/com/esprit/scluptfit/views/MainActivity.java
+++ app/src/main/java/com/esprit/scluptfit/views/MainActivity.java
@@ -9,13 +9,19 @@ import android.os.Handler;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.esprit.scluptfit.R;
 
 public class MainActivity extends AppCompatActivity {
-    private static  int SPLASH_SCREEN = 2000;
+    private static  int SPLASH_SCREEN = 5000;
+    //variables
+    Animation topAnim,bottomAnim;
     ImageView image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +29,19 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
         image =findViewById(R.id.img);
+        image.setAnimation(topAnim);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this,StartActivity.class);
-
-                startActivity(intent);
+                Pair[] pairs=new Pair[1];
+                pairs[0]=new Pair<View,String>(image,"logo_image");
+                ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+                finish();
             }
         },SPLASH_SCREEN) ;
 
