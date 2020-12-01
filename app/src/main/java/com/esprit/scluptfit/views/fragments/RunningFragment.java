e564f42f48f45c5caab3669c4224cb5f7bc93e11
wassimsefi <wassimsefi@gmail.com>
2020-12-01 14:54:33 +0100

    commmit post

--- app/src/main/java/com/esprit/scluptfit/views/fragments/RunningFragment.java
+++ app/src/main/java/com/esprit/scluptfit/views/fragments/RunningFragment.java
@@ -1,24 +1,132 @@
 package com.esprit.scluptfit.views.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.esprit.scluptfit.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
 
 
-public class RunningFragment extends Fragment {
+public class RunningFragment extends Fragment implements OnMapReadyCallback {
 
-
+    GoogleMap map;
+    SupportMapFragment mapFragment;
+    FusedLocationProviderClient client;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_running, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_running, container, false);
+
+        mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.google_map);
+
+        //Mon position
+
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            // getCurrentLocation
+            Task<Location> task = client.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if(location != null){
+
+                        mapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(GoogleMap googleMap) {
+                                LatLng latLng=new LatLng(location.getLatitude()
+                                        ,location.getLongitude());
+                                MarkerOptions options=new MarkerOptions().position(latLng)
+                                        .title("I am there");
+                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                                googleMap.addMarker(options);
+
+
+
+                            }
+                        });
+                    }
+                }
+            });
+        }else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
+
+
+        mapFragment.getMapAsync(this::onMapReady);
+
+        return view;    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map =googleMap;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==44){
+            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                    // getCurrentLocation
+                    Task<Location> task = client.getLastLocation();
+                    task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location != null){
+
+                                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                                    @Override
+                                    public void onMapReady(GoogleMap googleMap) {
+                                        LatLng latLng=new LatLng(location.getLatitude()
+                                                ,location.getLongitude());
+                                        MarkerOptions options=new MarkerOptions().position(latLng)
+                                                .title("I am there");
+                                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                                        googleMap.addMarker(options);
+
+
+
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            }
+        }
     }
 }
