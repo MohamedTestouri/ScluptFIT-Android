f114eba861c24d6a3b55be3803ba9d5f350722b2
Mohamed Testoury <mohamed.testouri@esprit.tn>
2020-12-06 12:10:21 +0100

    start

--- app/src/main/java/com/esprit/scluptfit/utils/RetrofitClientInstance.java
+++ app/src/main/java/com/esprit/scluptfit/utils/RetrofitClientInstance.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClientInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://192.168.43.253:6666";
+    private static final String BASE_URL = "https://sclupt-fit.herokuapp.com";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
